d6eee9b5c10935179d7c211926c9dc6c8e7d2049
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-12 08:14:22 +0000

    all done with UI

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), //maps @/ to /src direcotry
+      '@': path.resolve(__dirname, 'src'), //maps @/ to /src direcotry
     },
   },
 })
