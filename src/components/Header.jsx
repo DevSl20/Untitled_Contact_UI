6756052a146a216855fc6d90a603a7dd739031c7
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-12 07:54:29 +0000

    header upadated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 function Header() {
-    return (
-    <header className="mb-4 flex items-center gap-2">
+  return (
+    <header className="mb-8 flex items-center gap-2">
       <img src="favicon.png" alt="Untitled UI" className="w-10" />
-      <h1 className="font-bold text-4xl">Untitled UI</h1>
+      <p className="font-bold">Untitled UI</p>
     </header>
-    );
-  }
-  
-  export default Header;
+  );
+}
+
+export default Header;
