a5a5dd51c9c190c9e8f44cbf5eb82acd86750804
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-12 08:00:32 +0000

    all chages save

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,14 @@ import Form from "@/components/Form";
 
 function Home() {
   return (
-    <div className="min-h-screen w-full bg-zinc-100 px-4 py-2 md:px-8 md:py-4"> 
+    <div className="min-h-screen w-full bg-zinc-50 px-4 py-2 md:px-8 md:py-4">
       <Header />
+
       <div className="grid grid-cols-8">
-        <div className="col-span-2 hidden bg-red-300 md:block">
+        <div className="col-span-2 hidden md:block">
           <Aside />
         </div>
-        <div className="col-span-full bg-green-200 md:col-span-6">
+        <div className="col-span-full rounded-lg p-5 md:col-span-6 md:bg-lime-400">
           <Form />
         </div>
       </div>
